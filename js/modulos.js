90c8df7a9152b5a59349b5a0c79edad398fe6b0c
cristja <cristianjaramillo179@gmail.com>
2025-02-24 13:33:21 -0500

    mejora interfaz

---
+++ js/modulos.js
@@ -0,0 +1,15 @@
+const contenedor = document.querySelector("#contenedor")
+
+modulos.forEach(element => {
+    let card = document.createElement("div.col")
+    card.innerHTML = `
+    <div class="card opacity-75">
+        <img src="${element.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${element.titulo}</h5>
+            <p class="card-text pb-3">${element.descripcion}</p>
+        </div>
+    </div>
+    `
+    contenedor.appendChild(card)
+});
