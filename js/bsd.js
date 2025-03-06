465ba1ceb3cf5505a80d0291aff10da6eb4c41d3
AlexisHenao123 <brayanhenao913@gmail.com>
2025-03-06 13:14:36 -0500

    Parte test

--- js/bsd.js
+++ js/bsd.js
@@ -1,27 +1,27 @@
 let modulos = [
     {
-        titulo: "MODÚLO 1",
+        titulo: "MÓDULO 1",
         img: "https://assets.goal.com/images/v3/blt904c4fe3a0126f9d/Goal_HIC_-_LinkedIn_(15).jpg?auto=webp&format=pjpg&width=3840&quality=60",
-        descripcion: "Introducción a este curso y a todo el tema relacionado con las lesiones deportivas en el futbol"
+        descripcion: "Introducción a este curso y a todo el tema relacionado con las lesiones deportivas en el fútbol"
     },
     {
-        titulo: "MODÚLO 2",
+        titulo: "MÓDULO 2",
         img: "https://cdn.futbollab.com/storage/blog/seo/blog---prevencion-de-lesiones-de-isquiotibiales-2.png",
-        descripcion: "En este segundo modúlo podras encontrar: clasificacion, causales y tipos de lesiones"
+        descripcion: "En este segundo módulo podras encontrar: clasificación, causales y tipos de lesiones"
     },
     {
-        titulo: "MODÚLO 3",
+        titulo: "MÓDULO 3",
         img: "https://www.fcbarcelona.com/fcbarcelona/photo/2020/10/22/9f440cf5-48b6-405b-a6bb-1ff8dfa0fc19/foto2.Alimetacio-esport.jpg",
-        descripcion: "En este modúlo tendremos la importancia de la preparación, alimentación y implementación a la hora de desempeñarte en una cancha"
+        descripcion: "En este módulo tendremos la importancia de la preparación, alimentación y implementación a la hora de desempeñarte en una cancha"
     },
     {
-        titulo: "MODÚLO 4",
+        titulo: "MÓDULO 4",
         img: "https://www.elpais.com.co/resizer/v2/EW35G42ENNCM5GHG2DOVTJFNIQ.jpg?auth=3a2d6a1ed81898641ba6d01b5829045937691b282994fddba0aac2ae890898a4&smart=true&quality=75&width=1920&height=1080",
         descripcion: "Ya casi finalizando con tu proceso, tomaremos relevancia en el ambiente, con que indumentaria se contará, balones adecuados para la cancha y el entorno en el cual esta ubicada"
     },
     {
-        titulo: "MODÚLO 5",
+        titulo: "MÓDULO 5",
         img: "https://assets.goal.com/images/v3/blte9bb3ebe1aed15c4/trofeo-copa-sudamericana.jpg?auto=webp&format=pjpg&width=3840&quality=60",
-        descripcion: "Para finalizar, tendremos una evaluación que abarcará todo lo aprendido anteriormente y ver cuanto avanzaste en tu conocimiento, esto sera requerido para tu certificacion"
+        descripcion: "Para finalizar, tendremos una evaluación que abarcará todo lo aprendido anteriormente y ver cuanto avanzaste en tu conocimiento, esto sera requerido para tu certificación"
     }
 ]
